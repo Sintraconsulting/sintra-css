0fdb5d47b9ddd7540d711513dc803230f380a211
gimmyvd <g.vandeurse@sintraconsulting.eu>
2018-01-03 10:55:03 +0100

    Create main_v2.js

---
+++ 1ms/c/crearesitowebgratis/main_v2.js
@@ -0,0 +1,16 @@
+(function() {
+    "use strict";
+
+    if (typeof omsOnInit === 'function') {
+        omsOnInit(doWork);
+    }
+    else {
+        doWork();
+    }
+
+    function doWork() {
+        require(['jquery', 'domReady!'], function($) {
+            $( ".page_url_index  #contact_form_6 ).insertBefore( ".page_url_index #top_image" );
+        });
+    }
+})();
