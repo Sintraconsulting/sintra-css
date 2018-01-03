1c399dc921a0a9d3e2266eac7cce0834d5f549e3
gimmyvd <g.vandeurse@sintraconsulting.eu>
2018-01-03 13:42:25 +0100

    Update main_v2.js

--- 1ms/c/crearesitowebgratis/main_v2.js
+++ 1ms/c/crearesitowebgratis/main_v2.js
@@ -15,7 +15,7 @@
               innerForm = `<div class="form-box">${innerForm}</div>`;
               $('.contact-header').closest('.row')[0].innerHTML = innerForm;
 
-              $( '.form-box' ).insertBefore( '.top-image-content' );
+              $( '.form-box' ).insertAfter( '.top-image-content' );
               $('.contact-header').closest('.div_central').addClass('toRemove');
               $('.toRemove').remove();
            }
