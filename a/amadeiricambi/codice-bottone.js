50c72e8f85b9785f4b6a0a50b0ac04ac031ffdb5
verdesgrobert <github@appboxstudios.com>
2018-03-07 21:34:15 +0100

    Modifica Creazione back button

--- a/amadeiricambi/codice-bottone.js
+++ a/amadeiricambi/codice-bottone.js
@@ -26,7 +26,16 @@ function ExecuteWhenSelectorHasItems(baseSelector, selector, action,limit) {
     }
     check();
 }
-
+function createButton(content,href,onclick,css_class){
+    let backElem = document.createElement('a'),
+        backElemNode = document.createTextNode(content);
+    backElem.setAttribute('href', href);
+    if(onclick!=="")
+    	backElem.setAttribute('onclick', "window.history.back()");
+    backElem.classList.add(css_class, 'fa', 'fa-reply');
+    backElem.appendChild(backElemNode);
+    return backElem;
+}
 function modificaContenutoECreaBottoneIndietro(){
 	console.log("Ho iniziato a processare il contenuto");
   // Divide product name
@@ -61,36 +70,21 @@ function modificaContenutoECreaBottoneIndietro(){
   //Add back button on product page
   if(document.querySelector('body.ecommerce-product-page')) {
 	  console.log("Sto creando il bottone per pagina precedente");
-    let paginaPrec="Pagina Precedente";
-    
+   
     var isEnglish = document.querySelector(".lan_en.active");
     var isItalian = document.querySelector(".lan_it.active");
-    if (isEnglish) 
-        paginaPrec= "Previous Page";
-    else if (isItalian)
-        paginaPrec= "Pagina Precedente";
-
-    let backElem = document.createElement('a'),
-        backElemNode = document.createTextNode(paginaPrec);
-/* ho commentato questa roba
-,
-        currentSite = window.location.href,
-        backToReplace = currentSite.split('/').pop(),
-        backReplaced = currentSite.replace(`/${backToReplace}`, '.html');
-*/
-
-    //ho commentato questo
-    //backElem.setAttribute('href', backReplaced);
-    //ho aggiunto/modificato queste:
-    backElem.setAttribute('href', "#");
-    backElem.setAttribute('onclick', "window.history.back()");
-
-
-    backElem.classList.add('btn-back', 'fa', 'fa-reply');
-    backElem.appendChild(backElemNode);
+	  
+    let prevPage="Pagina Precedente",goToQuotation="Preventivo";
+    if (isEnglish) {
+        prevPage= "Previous Page";
+	goToQuotation = "Quotation";
+    }
+    let backElem = createButton(prevPage,"#","window.history.back()","btn-back");
+    document.querySelector('#page_title').appendChild(backElem);	
+    let preventivoElem=createButton(goToQuotation,"/preventivo.html","","btn-preventivo");
     document.querySelector('#page_title').appendChild(backElem);
   }else{
-	  console.log("Non ho trovato i dati 'body.ecommerce-product-page' per creare il bottone per pagina precedente");
+    console.log("Non ho trovato i dati 'body.ecommerce-product-page' per creare il bottone per pagina precedente");
   }
 }
 console.log("before setTimeout")
